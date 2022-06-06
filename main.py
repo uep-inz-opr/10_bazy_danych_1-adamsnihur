e17576dee670330c9c3f3db05a11f48a8f82fd8b
Adam Snihur <adam.snihur@gmail.com>
2022-06-06 16:25:17 +0200

    v1

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
+import pymysql	
+	
+connection = pymysql.connect(host = "ego.kie.ue.poznan.pl", port = 8080, user="inz_opr", passwd="inz_opr", database="inzynieria_oprogramowania")
+cursor = connection.cursor()
+
+cursor.execute("select * from polaczenia;")
+suma=0
+
+
+for i in cursor:
+	suma+=i[3]
+
 
 if __name__ == "__main__":
     pass
+
+
+print(suma)
